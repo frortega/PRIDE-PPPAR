b7451a81de4689f357f3fd78b0713bb1c185421b
PrideLab <48112921+PrideLab@users.noreply.github.com>
2026-07-12 15:07:46 +0800

    Update pdp3.sh

--- scripts/pdp3.sh
+++ scripts/pdp3.sh
@@ -58,7 +58,7 @@ readonly USECACHE=YES
 readonly USERTS=YES
 
 readonly SCRIPT_NAME="pdp3"
-readonly VERSION_NUM="3.2.9"
+readonly VERSION_NUM="3.2.10"
 
 ######################################################################
 ##                     System-specific Command                      ##
